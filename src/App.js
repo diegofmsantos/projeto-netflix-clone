eb3760c122b7a8aa700338548da06e475aa7d728
Diego Santos <diegofmsantos0@gmail.com>
2023-05-05 19:47:45 -0300

    fazendo a lista de filmes e o filme destaque

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { useEffect, useState } from "react";
+import "./App.css"
 import Tmdb from "./Tmdb";
 import { MovieList } from "./components/MoviesList/MovieList";
+import { FeaturedMovie } from "./components/FeaturedMovie/FeaturedMovie";
 
 const App = () => {
 
   const [moveList, setMoveList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
 
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList()
       setMoveList(list)
+
+        let originals = list.filter(i=>i.slug === 'originals')
+        let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+        let chosen = originals[0].items.results[randomChosen]
+        let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+        setFeaturedData(chosenInfo)
     }
 
     loadAll()
   }, [])
 
   return (
-    <div>
+    <div className="page">
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData}/>
+      }
+
       <section className="lists">
         {moveList.map((item, key) => (
           <MovieList key={key} title={item.title} items={item.items}/>
--- src/components/MoviesList/MovieList.js
+++ src/components/MoviesList/MovieList.js
@@ -1,15 +1,19 @@
-import React from "react"
+import { React } from "react"
 import "./MovieList.css"
 
-export const MovieList =  ({title, items}) => {
+export const MovieList = ({ title, items }) => {
 
     return (
-        <div>
+        <div className="movieList">
             <h2>{title}</h2>
-            <div className="listarea">
-                {items.results.length > 0 && items.results.map((item, key) => {
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
-                })}
+            <div className="movie-listarea">
+                <div className="movie-list">
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="movie-item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
